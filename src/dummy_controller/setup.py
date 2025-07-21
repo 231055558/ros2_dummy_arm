3432d7c10c1217a9d21cef37047de1c7ecf26553
231055558 <231055558@qq.com>
2025-07-21 11:08:30 +0800

    删除无用文件

--- src/dummy_controller/setup.py
+++ src/dummy_controller/setup.py
@@ -24,8 +24,6 @@ setup(
             'moveit_server = dummy_controller.moveit_server:main',
             'add_collision_object = dummy_controller.add_collision_object:main',
             'add_collision_object_simple = dummy_controller.add_collision_object_simple:main',
-            'test_trajectory = dummy_controller.test_trajectory:main',
-            'gazebo_joint_publisher = dummy_controller.gazebo_joint_publisher:main',
         ],
     },
 )
